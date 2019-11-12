0bef21f7879abf4c7767d27fe7afa27a61f86605
Darcoeur974 <darcoeur136dark@gmail.com>
2019-11-12 11:45:48 +0400

    MF : formulaire de contact (base)

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,20 @@
+<form onsubmit="send()" id="formContact">
+
+    <label for="nom">Nom et Prénom :</label>
+    <input type="text" name="nom" id="nom">
+
+    <label for="email">E-mail :</label>
+    <input type="email"  name="email" id="email">
+
+    <label for="objet">Objet :</label>
+    <input type="text" name="objet" id="objet">
+
+    <label for="message">Message :</label>
+    <textarea name="message" id="message"></textarea>
+
+    <input type="submit" value="Envoyer">
+
+</form>
+
+<script type="text/javascript" src="{{ asset('js/jquery-3.4.1.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', 'ContactController@index');
+Route::post('/contactPost', 'ContactController@envoiePost');
+
+// Route::get('/contact', 'ContactController@index');
