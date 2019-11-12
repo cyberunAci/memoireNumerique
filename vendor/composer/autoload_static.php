056273aa4f1d4a8048d625c9f553ea969f51c1bd
Darcoeur974 <darcoeur136dark@gmail.com>
2019-11-12 15:46:11 +0400

    MF : mise en place des regEx JS

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -401,6 +401,7 @@ class ComposerStaticInit75fb19882a5cd15ce53eaf96f0e68b3f
 
     public static $classMap = array (
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
+        'App\\ContactModel' => __DIR__ . '/../..' . '/app/ContactModel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Auth\\ConfirmPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ConfirmPasswordController.php',
         'App\\Http\\Controllers\\Auth\\ForgotPasswordController' => __DIR__ . '/../..' . '/app/Http/Controllers/Auth/ForgotPasswordController.php',
