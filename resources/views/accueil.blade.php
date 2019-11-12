9e3a449d9400581f66a6b320c91b271075d7b1e2
Benjamin <benjulienne0@gmail.com>
2019-11-12 11:55:39 +0400

    first commit benj

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+
+    <section class="accueil">
+        <div class="accueilDiv">
+            <h1>Presentation du projet</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius ipsam voluptatibus dolores consectetur quae exercitationem cumque temporibus impedit tempora voluptatum praesentium maiores eveniet esse, nostrum veniam asperiores, cupiditate quia. Sunt!</p>
+        </div>
+        <div class="accueilDiv">
+            <h1>Presentation de Cyberun</h1>
+            <p>Cyberun  est une association  qui met le numérique au service de la Solidarité et du développement  local durable.
+                Nos compétences numériques sont mobilisées pour servir la mémoire réunionnaise dans son ensemble. L’île étant l’épicentre de la rencontre entre différentes cultures, mais également  le creuset d’un patrimoine  naturel inégalé.
+                Notre démarche s’appuie sur une équipe soudée faisant valoir tout un faisceau de compétences diverses et mettant du cœur à l’ouvrage.</p>
+            <p>Qu’il s’agisse de sauvegarde de la mémoire, de tourisme solidaire, de réduction de la Fracture numérique ou encore de valorisation des initiatives durables, nos actions sont menées autours d’une seule et même ligne de conduite régissant nos vertus et nos  actions.</p>
+            <p>En ce sens, tous nos projets s’articulent  autour des notions d’innovation, de solidarité, de Tradition et de Divertissement. Site : <a href="http://cyberun.org/">Cyberun.org</a>. </p>
+        </div>
+    </section>
+@endSection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('accueil');
 });
 
 Route::get('/contact', 'ContactController@index');
