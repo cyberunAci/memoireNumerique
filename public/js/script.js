2a304835c706fdc0abb3b9dec3d6a4046505d873
Darcoeur974 <darcoeur136dark@gmail.com>
2019-11-13 15:23:36 +0400

    M : US13 ajout du regex youtube sur le lien, manque la partie courte et le blocage JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,6 @@
+// pour verification (en attente de trouver mieux)
+$verifEmail = "";
+$verifLien = "";
 /**
  * Envoie les informations du formulaire "contact"
  */
@@ -39,32 +42,37 @@ function sendContact() {
 function sendJeParticipe() {
     event.preventDefault();
 
-    let donnees = {
-        nom: $("#nomJeParticipe").val(),
-        email: $("#emailJeParticipe").val(),
-        titreVideo: $("#titreVideoJeParticipe").val(),
-        descriptionVideo: $("#descriptionVideoJeParticipe").val(),
-        lienVideo: $("#lienVideoJeParticipe").val(),
+    if ($verifLien) {
+        let donnees = {
+            nom: $("#nomJeParticipe").val(),
+            email: $("#emailJeParticipe").val(),
+            titreVideo: $("#titreVideoJeParticipe").val(),
+            descriptionVideo: $("#descriptionVideoJeParticipe").val(),
+            lienVideo: $("#lienVideoJeParticipe").val(),
+        }
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            method: "POST",
+            url: "/jeparticipe/message",
+            data: donnees,
+            dataType: "json"
+        })
+            .done(function (data) {
+                console.log(data);
+            })
+            .fail(function (status) {
+                $("#errorFormulaireJeParticipe").append('Les champs sont obligatoire ! code d\'erreur ' + status.status + '.');
+            })
+    } else {
+        console.log("erreur majeur!!! tout vas exploser!!")
     }
 
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-
-    $.ajax({
-        method: "POST",
-        url: "/jeparticipe/message",
-        data: donnees,
-        dataType: "json"
-    })
-        .done(function (data) {
-            console.log(data);
-        })
-        .fail(function (status) {
-            $("#errorFormulaireJeParticipe").append('Les champs sont obligatoire ! code d\'erreur ' + status.status + '.');
-        })
 
 }
 
@@ -73,12 +81,13 @@ function sendJeParticipe() {
  * @param {*} arg arg est l'élément this que vous trouverez dans le formulaire.
  */
 
-function regExp(arg) {    
-    champ = $("#" + arg.id ); //recuper l'input dans le formulaire
+function regExp(arg) {
+    champ = $("#" + arg.id); //recuper l'input dans le formulaire
     let regex = /^[\w|[\\-_ ](?![\\-_ ])|[\\u00C0\\u00C1\\u00C2\\u00C3\\u00C4\\u00C5\\u00C6\\u00C7\\u00C8\\u00C9\\u00CA\\u00CB\\u00CC\\u00CD\\u00CE\\u00CF\\u00D0\\u00D1\\u00D2\\u00D3\\u00D4\\u00D5\\u00D6\\u00D8\\u00D9\\u00DA\\u00DB\\u00DC\\u00DD\\u00DF\\u00E0\\u00E1\\u00E2\\u00E3\\u00E4\\u00E5\\u00E6\\u00E7\\u00E8\\u00E9\\u00EA\\u00EB\\u00EC\\u00ED\\u00EE\\u00EF\\u00F0\\u00F1\\u00F2\\u00F3\\u00F4\\u00F5\\u00F6\\u00F9\\u00FA\\u00FB\\u00FC\\u00FD\\u00FF\\u0153]]+$/;
-    
+
     if (regex.test(champ.val())) {
         champ.css("border", "2px green solid");
+        return true
     }
     else {
         champ.css("border", "2px red solid");
@@ -95,13 +104,32 @@ function regExpEmail(arg) {
         champ.css("border", "2px red solid");
     }
 }
+function regExpLien(arg) {
+    champ = $("#" + arg.id);
+    let regexLong = /^https:\/\/(www\.)?youtube\.com\/watch\?v=([\w-]+).*$/;
+    let regexLongNotSecure = /^http:\/\/(www\.)?youtube\.com\/watch\?v=([\w-]+).*$/;
+    let regexCourt = /^http:\/\/youtu\.be\/([\w-]+)$/;
 
+    if (regexLong.test(champ.val())) {
+        champ.css("border", "2px green solid");
+        $verifLien = true;
+    } else if (regexLongNotSecure.test(champ.val())) {
+        champ.css("border", "2px green solid");
+        $verifLien = true;
+    } else if (regexCourt.test(champ.val())) {
+        champ.css("border", "2px green solid");
+        $verifLien = true;
+    } else {
+        champ.css("border", "2px red solid");
+        $verifLien = false;
+    }
+}
 
 
 function sendVideo() {
     event.preventDefault();
 
-  
+
 
     $.ajaxSetup({
         headers: {
@@ -112,7 +140,7 @@ function sendVideo() {
     $.ajax({
         method: "POST",
         url: "/video/add",
-      
+
     })
         .done(function (data) {
             console.log(data);
@@ -128,5 +156,5 @@ function sendVideo() {
 
 function affichage(data) {
 
-    $(".complet").append("<p>" + data[0].titre  + "<br>" + data[0].resumer  + "<br>" + data[0].description + "<br> <img src='" + data[0].image + "' /></p>"  + "<iframe width='560' height='315' src='" + data[0].video + "' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+    $(".complet").append("<p>" + data[0].titre + "<br>" + data[0].resumer + "<br>" + data[0].description + "<br> <img src='" + data[0].image + "' /></p>" + "<iframe width='560' height='315' src='" + data[0].video + "' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
 }
