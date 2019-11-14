d5227bd2e446c988b5a74d26b2023254b84c7758
Benjamin <benjulienne0@gmail.com>
2019-11-14 13:26:58 +0400

    BF 07: Modification css, page description et VideoController

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.app')
 @section('content')
 <h1 class="text-center">CATEGORIE VIDEO</h1>
-
+    <div class="gallery js-flickity "data-flickity-options='{ "freeScroll": true, "wrapAround": true }'>
+        <div class="gallery-cell"></div>
+        <div class="gallery-cell"></div>
+        <div class="gallery-cell"></div>
+        <div class="gallery-cell"></div>
+        <div class="gallery-cell"></div>
+    </div>
 
     <div class="complet row text-center m-2 cateVideo">
 
