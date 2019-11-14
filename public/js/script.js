f0512a24a7e860fd9cd0f5c4626e450bc01d53e4
Darcoeur974 <darcoeur136dark@gmail.com>
2019-11-14 09:23:32 +0400

    M :US13 reprise

--- public/js/script.js
+++ public/js/script.js
@@ -7,20 +7,17 @@ $verifLien = "";
  */
 function sendContact() {
     event.preventDefault();
-
     let donnees = {
         nom: $("#nomContact").val(),
         email: $("#emailContact").val(),
         objet: $("#objetContact").val(),
         message: $("#messageContact").val(),
     }
-
     $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
     $.ajax({
         method: "POST",
         url: "/contact/message",
@@ -33,13 +30,10 @@ function sendContact() {
         .fail(function (status) {
             $("#errorFormulaireContact").append('Les champs sont obligatoire ! code d\'erreur ' + status.status + '.');
         })
-
 }
-
 /**
  * Envoie les informations du formulaire "je participe"
  */
-
 function sendJeParticipe() {
     event.preventDefault();
     if ($verifLien && $verif && $verifEmail) {
@@ -50,13 +44,11 @@ function sendJeParticipe() {
             descriptionVideo: $("#descriptionVideoJeParticipe").val(),
             lienVideo: $("#lienVideoJeParticipe").val(),
         }
-
         $.ajaxSetup({
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
-
         $.ajax({
             method: "POST",
             url: "/jeparticipe/message",
@@ -72,15 +64,11 @@ function sendJeParticipe() {
     } else {
         $("#errorFormulaireJeParticipe").append('Les champs sont INCORECT !');
     }
-
-
 }
-
 /**
  * zone de verification regex
  * @param {*} arg arg est l'élément this que vous trouverez dans le formulaire.
  */
-
 function regExp(arg) {
     champ = $("#" + arg.id); //recuper l'input dans le formulaire (nom, titre, description, etc / sauf mail et lien)(recuper l'ID)
     let regex = /^[\w|[\\-_ ](?![\\-_ ])|[\\u00C0\\u00C1\\u00C2\\u00C3\\u00C4\\u00C5\\u00C6\\u00C7\\u00C8\\u00C9\\u00CA\\u00CB\\u00CC\\u00CD\\u00CE\\u00CF\\u00D0\\u00D1\\u00D2\\u00D3\\u00D4\\u00D5\\u00D6\\u00D8\\u00D9\\u00DA\\u00DB\\u00DC\\u00DD\\u00DF\\u00E0\\u00E1\\u00E2\\u00E3\\u00E4\\u00E5\\u00E6\\u00E7\\u00E8\\u00E9\\u00EA\\u00EB\\u00EC\\u00ED\\u00EE\\u00EF\\u00F0\\u00F1\\u00F2\\u00F3\\u00F4\\u00F5\\u00F6\\u00F9\\u00FA\\u00FB\\u00FC\\u00FD\\u00FF\\u0153]]+$/;
@@ -120,8 +108,6 @@ function regExpLien(arg) { // regex pour les videos youtube
         $verifLien = false;
     }
 }
-
-
 function sendVideo() {
     event.preventDefault();
     $.ajaxSetup({
@@ -129,11 +115,9 @@ function sendVideo() {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-
     $.ajax({
         method: "POST",
         url: "/video/add",
-
     })
         .done(function (datas) {
             $.each(datas, function (index, data) {  // Appel la fonction affichage à chaque ligne
@@ -144,25 +128,13 @@ function sendVideo() {
         .fail(function (status) {
             console.log(status);
         })
-
 }
 function affichage(data) {
-<<<<<<< HEAD
-    $(".complet").append("<p>" + data[0].titre + "<br>" + data[0].resumer + "<br>" + data[0].description + "<br> <img src='" + data[0].image + "' /></p>" + "<iframe width='560' height='315' src='" + data[0].video + "' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
-}
-=======
-
     $(".complet").append("<div class='col-md-3'><p>" + data.titre + "<br>" + data.resumer + "<br>" + data.description + "<br> <img id='id" + data.id + "' src='" + data.image + "' /></p></div>");
     // $(".completModal").append("<div id='lol" + data[0].id + "' class='modal'><div class='modal-content'><span class='close'>&times;</span><p class='completModal'><iframe width='560' height='315' src='" + data[0].video + "' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe></p></div></div>");
-
     /* modal video */
-
     // Get the modal
-    
-
-
 }
 
 
 
->>>>>>> 9b3d98d721fc254f86c101e01e77c9d3d22cc1d4
