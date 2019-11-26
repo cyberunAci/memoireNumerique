678480461955f6a5b122d86af3a43c729275f8fa
kevin97426 <kevin.jams974@gmail.com>
2019-11-26 15:15:27 +0400

    final

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,14 +13,4 @@ const mix = require('laravel-mix');
 
 
 /*Compiler les fichiers scss en css pour la partie client*/
-mix.js('resources/js/client/affichage.js', 'public/js/client')
-.js('resources/js/client/carousel-video.js', 'public/js/client')
-.js('resources/js/client/contact.js', 'public/js/client')
-.js('resources/js/client/recherche.js', 'public/js/client')
-.js('resources/js/client/regex.js', 'public/js/client')
-.js('resources/js/admin/manageMemoires.js', 'public/js/admin')
-.js('resources/js/admin/remove.js', 'public/js/admin')
-.js('resources/js/app.js', 'public/js')
-.js('resources/js/bootstrap.js', 'public/js')
-.js('resources/js/jquery-3.4.1.js', 'public/js')
-.sass('resources/sass/client/app.scss', 'public/css/client');
+mix.sass('resources/sass/client/app.scss', 'public/css/client');
