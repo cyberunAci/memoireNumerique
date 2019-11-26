d980fb5de4f4246bfa403062c25e20e23f92f8b4
Benjamin <benjulienne0@gmail.com>
2019-11-26 15:18:44 +0400

    Benj creation des ressources

--- public/js/admin/remove.js
+++ public/js/admin/remove.js
@@ -1,34 +1,5 @@
 //Supprimer une memoire
 function supprimerMemoire() {
-<<<<<<< HEAD
-  $.ajax({
-    url: "/memoire/supprimer",
-    method: "GET",
-    data: {
-      id: id
-    },
-    dataType: "json"
-  }).done(function (data) {
-    console.log(data);
-  });
-}
-
-/***/ }),
-
-/***/ 6:
-/*!********************************************!*\
-  !*** multi ./resources/js/admin/remove.js ***!
-  \********************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-module.exports = __webpack_require__(/*! /home/cyberun-4/Bureau/PROJET/projetLaravel/memoireNumerique/resources/js/admin/remove.js */"./resources/js/admin/remove.js");
-
-
-/***/ })
-
-/******/ });
-=======
     $.ajax({
         url: "/memoire/supprimer",
         method: "GET",
@@ -40,4 +11,3 @@ module.exports = __webpack_require__(/*! /home/cyberun-4/Bureau/PROJET/projetLar
        console.log(data);
     })
 }
->>>>>>> b0775abb6ce777f10fec508187eebdae2953286e
