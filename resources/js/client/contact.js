6795349708845aba18814bc9a5007d885989b18f
Darcoeur974 <darcoeur136dark@gmail.com>
2019-11-26 14:47:41 +0400

    Correction Sass

--- resources/js/client/contact.js
+++ resources/js/client/contact.js
@@ -79,39 +79,3 @@ function sendJeParticipe() {
 /**
  * Envoie les informations du formulaire "je participe"
  */
-function sendJeParticipe() {
-    event.preventDefault();
-    if ($verifLien && $verif && $verifEmail) {
-        let donnees = {
-            nom: $("#nomJeParticipe").val(),
-            email: $("#emailJeParticipe").val(),
-            titreVideo: $("#titreVideoJeParticipe").val(),
-            descriptionVideo: $("#descriptionVideoJeParticipe").val(),
-            lienVideo: $("#lienVideoJeParticipe").val(),
-        }
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $.ajax({
-            method: "POST",
-            url: "/jeparticipe/message",
-            data: donnees,
-            dataType: "json"
-        })
-            .done(function (data) {
-                $("#errorFormulaire").empty();
-                $("#errorFormulaire").removeClass("errorFormulaire");
-                $("#errorFormulaire").append('<p>Votre Message a bien été envoyée !</p>');
-                $("#errorFormulaire").addClass("bonFormulaire");
-            })
-            .fail(function (status) {
-                $("#errorFormulaireJeParticipe").append('Code d\'erreur ' + status.status + '.');
-            })
-    } else {
-        $("#errorFormulaire").empty();
-        $("#errorFormulaire").append('<p>Attention! Les champs ne sont pas valide !</p>');
-        $("#errorFormulaire").addClass("errorFormulaire");
-    }
-}
